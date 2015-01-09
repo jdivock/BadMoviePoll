7467cf62138d110d7d76f579256f3909fb13b557
Jason Divock <jdivock@gmail>
2015-01-09 12:07:56 -0500

    initial setup and getting react going with systemjs

---
+++ lib/components/BadMoviePoll.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var BadMoviePoll = React.createClass({
+
+	render: function() {
+		return (
+			<div>Hi</div>
+		);
+	}
+
+});
+
+export default BadMoviePoll;
---
+++ lib/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BadMoviePoll from 'lib/components/BadMoviePoll.jsx!';
+
+React.render(<BadMoviePoll></BadMoviePoll>, document.getElementById('app'));
+
+console.log(React);
