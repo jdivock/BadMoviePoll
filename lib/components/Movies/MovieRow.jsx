7bf681118cb9f6765ffedd23dfbf48cc9e003c7f
Jason Divock <jdivock@gmail>
2015-01-14 16:38:47 -0500

    RT link added to movie results

--- lib/components/Movies/MovieRow.jsx
+++ lib/components/Movies/MovieRow.jsx
@@ -4,7 +4,6 @@ import debug from 'debug';
 
 let log = debug('BadMoviePoll:MovieRow.jsx');
 
-
 var MovieRow = React.createClass({
 
 	render: function(){
@@ -15,8 +14,12 @@ var MovieRow = React.createClass({
 					movie={this.props.movie}
 					clearResults={this.props.clearResults}>
 				</VoteCell>
-				<td><img src={this.props.movie.posters.thumbnail}/></td>
-				<td>{this.props.movie.title} ({this.props.movie.year})</td>
+				<td>
+					<a href={this.props.movie.links.alternate} target="_blank">
+						<img src={this.props.movie.posters.thumbnail}/>
+						{this.props.movie.title} ({this.props.movie.year})
+					</a>
+				</td>
 				<td>{this.props.movie.ratings.critics_rating}</td>
 			</tr>
 		);
