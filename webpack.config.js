24d9eb08acc0435cbbb655d6ee9bdf3192f7e3eb
Jason Divock <jdivock@gmail.com>
2015-01-18 15:00:50 -0500

    got f-a working, but it's a bit fragile with the directory path, I'll migrate to something else eventually, but suppose this was good practice. Besides, pissed the whole day away playign/fighting with webpack might as well keep going

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,12 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{test: /\.jsx$/, loader: 'traceur?runtime!jsx-loader'},
-			{test: /\.js$/, exclude: /node_modules/, loader: 'traceur?runtime'},
-			{test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-			{test: /\.css$/, loader: 'style-loader!css-loader'}
+		{test: /\.jsx$/, loader: 'traceur?runtime!jsx-loader'},
+		{test: /\.js$/, exclude: /node_modules/, loader: 'traceur?runtime'},
+		{test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+		{test: /\.css$/, loader: 'style-loader!css-loader'},
+		{test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+		{test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 		]
 	},
 	resolve: {
